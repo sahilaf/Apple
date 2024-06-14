4e795e3f466bd546711801e077c6d6a769929adc
sahilaf <sahilfarib320@gmail.com>
2024-06-15 00:41:54 +0600

    remove

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-wln",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true
